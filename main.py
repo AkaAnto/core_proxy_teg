d8b0c13dc877a04ea983e9994c0405afccae5ffa
Antonio Rodriguez <antojrd@gmail.com>
2021-10-17 11:38:02 -0500

    More debug

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
 db = SQLAlchemy(app)
-db.create_engine(Config.SQLALCHEMY_DATABASE_URI)
+db.engine_from_config("config.Config")
