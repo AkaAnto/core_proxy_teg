7833d1f446967c42a9880e34058eca7133e8614f
Antonio Rodriguez <antojrd@gmail.com>
2021-10-17 11:42:47 -0500

    try connecction

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
 db = SQLAlchemy(app)
-db.engine_from_config("config.Config")
+db.create_engine(Config.SQLALCHEMY_DATABASE_URI, SQLAlchemy.SQLALCHEMY_ENGINE_OPTIONS)
