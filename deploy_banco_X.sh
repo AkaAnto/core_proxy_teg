fbaa51911da044f2ca2856f04d37bbb69d09ac21
antonio rodriguez <antonio.rodriguez@gerdau.com>
2022-10-16 15:31:38 -0500

    fixes env

--- deploy_banco_X.sh
+++ deploy_banco_X.sh
@@ -1,6 +1,3 @@
-source .env
-source .env.sqlserver
-export USE_DATABASE="sqlserver"
 docker system prune -a -f
 docker build --build-arg MY_SERVICE_PORT=5000 --build-arg MY_SERVICE_URL=0.0.0.0:5000  -t banco_x .
 docker run -d -t banco_x
