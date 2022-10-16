6c615c8b7c32f902c867d2c173d0cd38737a0b4d
antonio rodriguez <antonio.rodriguez@gerdau.com>
2022-10-16 17:16:48 -0500

    deploy bank y

--- deploy_banco_Y.sh
+++ deploy_banco_Y.sh
@@ -1,5 +1,3 @@
-source .env
-export USE_DATABASE="mysql"
 docker system prune -a -f
-docker build --build-arg MY_SERVICE_PORT=8000 --build-arg MY_SERVICE_URL=0.0.0.0:8000 -t banco_y .
-docker run -d -t banco_y
+docker build -t banco_y .
+docker run -p 8000:5000 -d -t banco_y
